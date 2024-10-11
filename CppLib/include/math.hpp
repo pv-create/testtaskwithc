1dfc3967a2df5a302a9c8fa30f8f1fcb46b46e12
pavel <pavel_vilkov17@mail.ru>
2024-10-11 21:34:42 +0700

    feature: добавлена математическая библиотека на с++

---
+++ CppLib/include/math.hpp
@@ -0,0 +1,11 @@
+#pragma 
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+int fibonacci(int n);
+
+#ifdef __cplusplus
+}
+#endif
---
+++ CppLib/src/math.cpp
@@ -0,0 +1,8 @@
+#include "math.hpp"
+
+int fibonacci(int n){
+        if (n <= 1) return 1;
+        return ( fibonacci(n - 1) + fibonacci(n - 2) );
+}
+
+
